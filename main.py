5c90603c6bc6d905243c38eb22cc7389ea7d2b00
jess <doing.easiest_0s@icloud.com>
2025-11-19 15:04:47 +0000

    change

--- main.py
+++ main.py
@@ -4,23 +4,20 @@ import requests
 import tweepy
 
 
-# Pick a random image/video from the 'assets' folder
 def get_random_media():
-    path = 'assets' # 
+    path = 'assets' 
     objects = os.listdir(path)
 
     media = random.choice(objects)
     return os.path.join(path, media)
 
 
-# Authorize Twitter with v1.1 API
 def auth_v1(consumer_key, consumer_secret, access_token, access_token_secret):
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     return tweepy.API(auth)
 
 
-# Authorize Twitter with v2 API
 def auth_v2(consumer_key, consumer_secret, access_token, access_token_secret):
     return tweepy.Client(
         consumer_key=consumer_key, consumer_secret=consumer_secret,
@@ -29,9 +26,7 @@ def auth_v2(consumer_key, consumer_secret, access_token, access_token_secret):
     )
 
 
-# Tweet picked image/video
 def tweet(media) -> requests.Response:
-    # In the next for 4 lines we are getting the keys from Step 4
     consumer_key = os.environ['CONSUMER_KEY']
     consumer_secret = os.environ['CONSUMER_SECRET']
     access_token = os.environ['ACCESS_TOKEN']
